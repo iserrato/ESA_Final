7a7c541a9345d31105ead76fe24294aec3869e63
Isabel Serrato <iserrato@olin.edu>
2021-05-04 23:25:48 -0400

    Create method to decode message
    Filtering is still not working. might want to change the conv so the final answer is the right length we want it to be

--- modem_rx_starter.m
+++ modem_rx_starter.m
@@ -23,40 +23,57 @@ x = y_t(1: 4100) .* cos(2*pi*(f_c/Fs)*[0:4099]');
 
 Xw = fft(x);
 O = linspace(-pi, pi - 2*pi/length(x), length(x));
-figure(1)
-plot(O, abs(Xw))
-title('ft of x')
+% figure(1)
+% plot(O, abs(Xw))
+% title('FFT of x')
 
 Y = fft(y_t);
 O2 = linspace(-pi, pi - 2*pi/length(Y), length(Y));
-figure(7)
-plot(O2,abs(Y))
-title('ft of yt')
+% figure(2)
+% plot(O2,abs(Y))
+% title('FFT of yt')
 
 t = [-100:1:99]*(1/Fs);
-W = 2*pi*1000; 
+W = 2*pi; 
 h = W/pi*sinc(W/pi*t);
 % figure(3)
 % plot(t, h)
 
-m = conv(x, h); 
-figure(2)
+m = conv(x, h);
+figure(4)
 plot(m)
 xlim([0 4500])
+title('Plot of m, reconstructed signal')
 
 N = length(m);
 X = fftshift(fft(m));
 f = linspace(-Fs/2*2*pi, 2*pi*Fs/2- 2*pi*Fs/length(m), length(m));
-figure(6)
+figure(5)
 plot(f, abs(X));
-xlabel('Frequency (rad/s)');
-ylabel('|X(j\omega)|');
+% title('FFT of M(jw)')
+% xlabel('Frequency (rad/s)');
+% ylabel('|M(j\omega)|');
 
+mpos = find(m>0);
+mneg = find(m<0);
 
+m(mpos) = 1;
+m(mneg) = 0;
+figure(6)
+plot(m)
+title('Restructured 1s and 0s from m')
+
+x_d = downsample(m, 100);
+figure(7)
+plot(x_d)
+title('Downsampled x')
+x_d = x_d(1: msg_length*8);
+length(x_d)
+x_d = dec2bin(x_d);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % convert to a string assuming that x_d is a vector of 1s and 0s
 % representing the decoded bits
-% BitsToString(x_d)
+BitsToString(x_d)
 
